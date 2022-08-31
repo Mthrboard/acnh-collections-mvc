a917615c6ff408db9a4fb9365c44e5ec4f16b9aa
ChrisThompsonDev <ChrisThompsonDev@protonmail.com>
2022-08-31 15:03:35 -0400

    first commit

---
+++ public/js/main.js
@@ -0,0 +1,70 @@
+/* const deleteBtn = document.querySelectorAll('.del') */
+const todoItem = document.querySelectorAll('div')
+const todoComplete = document.querySelectorAll('div.completed')
+
+/* Array.from(deleteBtn).forEach((el)=>{
+    el.addEventListener('click', deleteTodo)
+}) */
+
+Array.from(todoItem).forEach((el)=>{
+    el.addEventListener('click', markComplete)
+})
+
+Array.from(todoComplete).forEach((el)=>{
+    el.addEventListener('click', markIncomplete)
+})
+
+/* async function deleteTodo(){
+    const todoId = this.parentNode.dataset.id
+    try{
+        const response = await fetch('todos/deleteTodo', {
+            method: 'delete',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({
+                'todoIdFromJSFile': todoId
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    }catch(err){
+        console.log(err)
+    }
+} */
+
+async function markComplete(){
+    const itemId = this.id
+    /* try{
+        const response = await fetch('todos/markComplete', {
+            method: 'put',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({
+                'todoIdFromJSFile': itemId
+            })
+        })
+        const data = await response.json()
+        console.log(itemId)
+        location.reload()
+    }catch(err){
+        console.log(err)
+    } */
+    console.log(itemId)
+}
+
+async function markIncomplete(){
+    const todoId = this.parentNode.dataset.id
+    try{
+        const response = await fetch('todos/markIncomplete', {
+            method: 'put',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({
+                'todoIdFromJSFile': todoId
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    }catch(err){
+        console.log(err)
+    }
+}
